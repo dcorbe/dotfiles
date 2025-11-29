1e7ffbd6b37e2085144097b78b495e86536b95a6
Daniel Corbe <daniel@corbe.net>
2025-11-29 02:19:12 -0500

    Enable spell checking for git commit messages

--- nvim/init.lua
+++ nvim/init.lua
@@ -64,7 +64,14 @@ vim.diagnostic.config({
     end,
   })
 
-
+  -- Enable spell checking for git commits
+  vim.api.nvim_create_autocmd("FileType", {
+    pattern = "gitcommit",
+    callback = function()
+      vim.opt_local.spell = true
+      vim.opt_local.spelllang = 'en_us'
+    end,
+  })
 
 
 -- This comes last
